26fedb03495e3523a01944b6f317baec05fe6382
Azizbek <azizbeknarzullayev779@gmail.com>
2023-05-17 16:13:01 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,303 @@
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+//     );
+//   }
+// }
+//
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, required this.title});
+//
+//   final String title;
+//
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//           child: Row(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: [
+//           FloatingActionButton(
+//             onPressed: () {},
+//             child: const Icon(Icons.add),
+//           ),
+//           const SizedBox(width: 10),
+//           FloatingActionButton(
+//             onPressed: () {},
+//             child: const Text("2 ^ N"),
+//           ),
+//         ],
+//       )),
+//     );
+//   }
+// }
+
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+void main() => runApp(const MaterialApp(home: MyHome()));
+
+class MyHome extends StatelessWidget {
+  const MyHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter Demo Home Page')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => const QRViewExample(isAdd: true),
+                ));
+              },
+              child: const Text('N1+N2'),
+            ),
+          ),
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => const QRViewExample(isAdd: false),
+                ));
+              },
+              child: const Text('2^N'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class QRViewExample extends StatefulWidget {
+  final bool isAdd;
+
+  const QRViewExample({Key? key, required this.isAdd}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _QRViewExampleState();
+}
+
+class _QRViewExampleState extends State<QRViewExample> {
+  Barcode? result;
+  QRViewController? controller;
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  BigInt? number1;
+  BigInt? number2;
+  int? number3;
+  List<String>? resultList;
+  bool scanner = false;
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller!.pauseCamera();
+    }
+    controller!.resumeCamera();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    number2 = null;
+    number1 = null;
+    number3 = null;
+    resultList?.clear();
+    controller?.dispose();
+    scanner = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Expanded(flex: 4, child: _buildQrView(context)),
+          SizedBox(
+            height: 150,
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Container(
+                  height: 30,
+                  margin: const EdgeInsets.symmetric(horizontal: 8),
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        if (result != null && scanner)
+                          if (widget.isAdd)
+                            Text(
+                              '${describeEnum(result!.format)}  \nData: N1:$number1 + N2:$number2 = ${number1! + number2!}',
+                              style: const TextStyle(fontSize: 13),
+                            )
+                          else
+                            Text('${describeEnum(result!.format)}  \nData: 2^$number3=${BigInt.from(2).pow(number3!)}', style: const TextStyle(fontSize: 13))
+                        else
+                          const Text('Scan a code', style: TextStyle(fontSize: 13)),
+                      ],
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      height: 30,
+                      margin: const EdgeInsets.all(8),
+                      child: ElevatedButton(
+                          onPressed: () async {
+                            await controller?.toggleFlash();
+                            setState(() {});
+                          },
+                          child: FutureBuilder(
+                            future: controller?.getFlashStatus(),
+                            builder: (context, snapshot) {
+                              return Text('Flash: ${snapshot.data}');
+                            },
+                          )),
+                    ),
+                    Container(
+                      height: 30,
+                      margin: const EdgeInsets.all(8),
+                      child: ElevatedButton(
+                          onPressed: () async {
+                            await controller?.flipCamera();
+                            setState(() {});
+                          },
+                          child: FutureBuilder(
+                            future: controller?.getCameraInfo(),
+                            builder: (context, snapshot) {
+                              if (snapshot.data != null) {
+                                return Text('Camera facing ${describeEnum(snapshot.data!)}');
+                              } else {
+                                return const Text('loading');
+                              }
+                            },
+                          )),
+                    )
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      height: 30,
+                      margin: const EdgeInsets.all(8),
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          await controller?.pauseCamera();
+                        },
+                        child: const Text('pause', style: TextStyle(fontSize: 20)),
+                      ),
+                    ),
+                    Container(
+                      height: 30,
+                      margin: const EdgeInsets.all(8),
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          await controller?.resumeCamera();
+                        },
+                        child: const Text('resume', style: TextStyle(fontSize: 20)),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildQrView(BuildContext context) {
+    var scanArea = (MediaQuery.of(context).size.width < 400 || MediaQuery.of(context).size.height < 400) ? 150.0 : 300.0;
+    return QRView(
+      key: qrKey,
+      onQRViewCreated: _onQRViewCreated,
+      overlay: QrScannerOverlayShape(borderColor: Colors.red, borderRadius: 10, borderLength: 30, borderWidth: 10, cutOutSize: scanArea),
+      // onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    setState(() {
+      this.controller = controller;
+    });
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+        print('_QRViewExampleState._onQRViewCreated');
+        if (result != null) {
+          if (widget.isAdd) {
+            if (result!.code!.length <= 200) {
+              resultList = (result!.code!.toString()).split(r'\n');
+              if (resultList!.length == 2) {
+                number1 = BigInt.parse(resultList![0]);
+                number2 = BigInt.parse(resultList![1]);
+                scanner = true;
+              } else {
+                snackBar(r'The value returned by the scanner does not have numbers separated by the "\n" character');
+              }
+            }
+          }
+          else {
+            if (int.parse(result!.code!) <= 10000) {
+              result = scanData;
+              number3 = int.parse(result!.code!);
+              scanner = true;
+            }
+            else {
+              snackBar('The number on the scanner is greater than 10,000');
+            }
+          }
+        }
+        else {
+          snackBar("There was no information from the scanner");
+        }
+      });
+    });
+  }
+
+  snackBar(String content) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      duration: const Duration(milliseconds: 100),
+      backgroundColor: Colors.red,
+      content: Text(content),
+    ));
+  }
+}
